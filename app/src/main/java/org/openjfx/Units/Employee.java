4704b444ef786bb5d6ed23fed678c64b3862f0b8
Madelyn Hubbard <mhubbard@uvic.ca>
2022-11-09 16:08:36 -0800

    Refactored Employee class to have a list of patients they're responsible for

--- app/src/main/java/org/openjfx/Units/Employee.java
+++ app/src/main/java/org/openjfx/Units/Employee.java
@@ -1,16 +1,36 @@
 package org.openjfx.Units;
 
+import java.util.LinkedList;
+
 public class Employee extends Person 
 {
+    private LinkedList<Patient> patients;
+
     public Employee(String fn, String ln, String id, int year, int month, int day)
     {
         setName(fn, ln);
         setEmployeeID(id);
         setDOB(year, month, day);
+        this.patients = new LinkedList<Patient>();
     }
 
-    void setEmployeeID(String id)
+    public void setEmployeeID(String id)
     {
         setID(id);
+    }
+
+    public void addPatient(Patient newPatient)
+    {
+        this.patients.add(newPatient);
+    }
+
+    public void removePatient(Patient patientToRemove)
+    {
+        this.patients.remove(patientToRemove);
+    }
+
+    public LinkedList<Patient> getPatientList()
+    {
+        return this.patients;
     }
 }
